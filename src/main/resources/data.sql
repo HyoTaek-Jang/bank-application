32b04b068c6e10a0f028e00cc60ba5c270ccebbc
HyoTaek-Jang <hyotaek9812@gmail.com>
2022-02-17 22:31:08 +0900

    feat: account sql init 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,5 @@ INSERT INTO user (name) SELECT '신민아' FROM DUAL WHERE NOT EXISTS (SELECT * 
 INSERT INTO user (name) SELECT '천우희' FROM DUAL WHERE NOT EXISTS (SELECT * FROM user WHERE name = '천우희');
 INSERT INTO user (name) SELECT '김당근' FROM DUAL WHERE NOT EXISTS (SELECT * FROM user WHERE name = '김당근');
 INSERT INTO user (name) SELECT '장캐럿' FROM DUAL WHERE NOT EXISTS (SELECT * FROM user WHERE name = '장캐럿');
+
+INSERT INTO account(account_id, account_number, bank_id, user_id) SELECT 12345678, 1234567890, 1, 1 FROM DUAL WHERE NOT EXISTS (SELECT * FROM account WHERE account_number = 1234567890);
